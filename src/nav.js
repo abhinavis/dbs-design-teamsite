aec94b4a8acf8507c6dafcf81ec569a71196127a
xdeepakv <deepak.m.shrma@gmail.com>
2020-03-03 02:23:16 +0800

    remove duplicate and clean html

--- src/nav.js
+++ src/nav.js
@@ -1,33 +1,26 @@
-import { link } from "fs";
-import SwupScrollPlugin from '@swup/scroll-plugin';
-
-// const swup = new Swup();
-const swup = new Swup({
-    plugins: [new SwupScrollPlugin()]
-  });
-
-  
+new Swup({
+  plugins: [new SwupScrollPlugin()]
+});
 
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 
-hamburger.addEventListener('click', ()=>{
-    //Hamburger Animation
-    hamburger.classList.toggle("toggle");
-   //Animate Links
-    navLinks.classList.toggle("open");
-    links.forEach(link => {
-        link.classList.toggle("fade");
-    });
+console.log(hamburger)
+hamburger.addEventListener("click", () => {
+  //Hamburger Animation
+  hamburger.classList.toggle("toggle");
+  //Animate Links
+  navLinks.classList.toggle("open");
+  links.forEach(link => {
+    link.classList.toggle("fade");
+  });
 });
 
-navLinks.addEventListener('click',()=>{
-    navLinks.classList.toggle("open");
-    links.forEach(link => {
-        link.classList.toggle("fade");
-    });
-    hamburger.classList.toggle("toggle");
+navLinks.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  links.forEach(link => {
+    link.classList.toggle("fade");
+  });
+  hamburger.classList.toggle("toggle");
 });
-
-
